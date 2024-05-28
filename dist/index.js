a315b0730a2e8619b15c3fe904d4676958abb3e1
Ashish <theashish752@gmail.com>
2024-05-28 10:39:25 +0530

    CRUD methods

--- dist/index.js
+++ dist/index.js
@@ -11,26 +11,36 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 const client_1 = require("@prisma/client");
 const prisma = new client_1.PrismaClient();
-//  -----------  To update the user data with the id
-function UpdateUser(username_1, _a) {
-    return __awaiter(this, arguments, void 0, function* (username, { email, password }) {
-        const res = yield prisma.user.update({
-            where: { id: 1 },
-            data: {
-                email,
-                password,
-            },
-        });
-        console.log(res);
-    });
-}
-UpdateUser('ashish', {
-    email: 'theashish752@gmail.com',
-    password: 'AshishSingh752'
-});
-// ------------ To insert the data into the userTable
-// async function insertUser(username: string, email: string, password: string) {
-//   const res = await prisma.user.create({
+// ! To delete the user by it's  Id
+// async function deleteUser(id:any){
+//   try {
+//         const users = await prisma.user.delete(id);
+//         console.log('Deleted User:', users);
+//       } catch (error) {
+//         console.error('Error deleting users:', error);
+//       } finally {
+//         await prisma.$disconnect();  
+//       }
+// }
+// deleteUser(1);
+//! to get the user details from the database
+// async function findAllUsers() {
+//   try {
+//     const users = await prisma.user.findMany();
+//     console.log('All Users:', users);
+//   } catch (error) {
+//     console.error('Error retrieving users:', error);
+//   } finally {
+//     await prisma.$disconnect();  
+//   }
+// }
+// findAllUsers();
+//!  -----------  To update the user data with the id
+// async function UpdateUser(
+//   {username, email, password }: UpdateUserInterface
+// ) {
+//   const res = await prisma.user.update({
+//     where: { id:1 },
 //     data: {
 //       username,
 //       email,
@@ -39,4 +49,22 @@ UpdateUser('ashish', {
 //   });
 //   console.log(res);
 // }
-// insertUser("ashish", "ashishsingh.nitr@gmail.com ", "AshishSingh");
+// UpdateUser({
+//     username:'Ashish Singh',
+//     email:'theashish752@gmail.com',
+//     password:'Ashish752'
+// });
+//! ------------ To insert the data into the userTable
+function insertUser(username, email, password) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const res = yield prisma.user.create({
+            data: {
+                username,
+                email,
+                password,
+            },
+        });
+        console.log(res);
+    });
+}
+insertUser("Pushpa", "Pushpa@gmail.com ", "Pushpa");
--- src/index.ts
+++ src/index.ts
@@ -4,38 +4,69 @@ const prisma = new PrismaClient();
 interface UpdateUserInterface {
   email: string;
   password: string;
+  username: string,
 }
 
-//!  -----------  To update the user data with the id
-// async function UpdateUser(
-//   username: string,
-//   { email, password }: UpdateUserInterface
-// ) {
-//   const res = await prisma.user.update({
-//     where: { id:1 },
-//     data: {
-//       email,
-//       password,
-//     },
-//   });
-//   console.log(res);
-// }
 
-// UpdateUser('ashish',{
-//     email:'theashish752@gmail.com',
-//     password:'AshishSingh752'
-// });
+//! To delete the user by it's  Id
+async function deleteUser(id:any){
+  try {
+        const users = await prisma.user.delete(id);
+        console.log('Deleted User:', users);
+      } catch (error) {
+        console.error('Error deleting users:', error);
+      } finally {
+        await prisma.$disconnect();  
+      }
+}
+deleteUser(1);
+
+
+//! to get the user details from the database
+async function findAllUsers() {
+  try {
+    const users = await prisma.user.findMany();
+    console.log('All Users:', users);
+  } catch (error) {
+    console.error('Error retrieving users:', error);
+  } finally {
+    await prisma.$disconnect();  
+  }
+}
+findAllUsers();
+
+
+//!  -----------  To update the user data with the id
+async function UpdateUser(
+  {username, email, password }: UpdateUserInterface
+) {
+  const res = await prisma.user.update({
+    where: { id:1 },
+    data: {
+      username,
+      email,
+      password,
+    },
+  });
+  console.log(res);
+}
+
+UpdateUser({
+    username:'Ashish Singh',
+    email:'theashish752@gmail.com',
+    password:'Ashish752'
+});
 
 //! ------------ To insert the data into the userTable
-// async function insertUser(username: string, email: string, password: string) {
-//   const res = await prisma.user.create({
-//     data: {
-//       username,
-//       email,
-//       password,
-//     },
-//   });
-//   console.log(res);
-// }
+async function insertUser(username: string, email: string, password: string) {
+  const res = await prisma.user.create({
+    data: {
+      username,
+      email,
+      password,
+    },
+  });
+  console.log(res);
+}
 
-// insertUser("ashish", "ashishsingh.nitr@gmail.com ", "AshishSingh");
+insertUser("Pushpa", "Pushpa@gmail.com ", "Pushpa");
